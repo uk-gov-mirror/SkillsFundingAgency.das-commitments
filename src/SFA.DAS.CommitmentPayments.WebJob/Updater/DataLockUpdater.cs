569a8e19dc7a322af1f05fe01d61054b70e8621a
phildavies <github@phildavies.co.uk>
2017-12-20 10:52:34 +0000

    add my own notes and todos (which will be removed/tidied before merging back to master) + some annoying auto-layout changes i'm not gonna fight + the change from part 1 of the optimisation (will merge from the branch)

--- src/SFA.DAS.CommitmentPayments.WebJob/Updater/DataLockUpdater.cs
+++ src/SFA.DAS.CommitmentPayments.WebJob/Updater/DataLockUpdater.cs
@@ -12,6 +12,7 @@ using SFA.DAS.Commitments.Domain.Exceptions;
 
 namespace SFA.DAS.CommitmentPayments.WebJob.Updater
 {
+    //todo: interface is in same assembly (entourage), utilise staircase pattern? not worth it as probably nothing external uses these classes
     public class DataLockUpdater : IDataLockUpdater
     {
         private readonly ILog _logger;
@@ -33,15 +34,15 @@ namespace SFA.DAS.CommitmentPayments.WebJob.Updater
             IFilterOutAcademicYearRollOverDataLocks filter,
             IApprenticeshipRepository apprenticeshipRepository)
         {
-            if(logger==null)
+            if (logger == null)
                 throw new ArgumentNullException(nameof(ILog));
-            if (paymentEventsService== null)
+            if (paymentEventsService == null)
                 throw new ArgumentNullException(nameof(IPaymentEvents));
-            if(dataLockRepository==null)
+            if (dataLockRepository == null)
                 throw new ArgumentNullException(nameof(IDataLockRepository));
-            if(apprenticeshipUpdateRepository == null)
+            if (apprenticeshipUpdateRepository == null)
                 throw new ArgumentNullException(nameof(IApprenticeshipUpdateRepository));
-            if(config == null)
+            if (config == null)
                 throw new ArgumentNullException(nameof(CommitmentPaymentsConfiguration));
             if (filter == null)
                 throw new ArgumentNullException(nameof(IFilterOutAcademicYearRollOverDataLocks));
@@ -71,6 +72,8 @@ namespace SFA.DAS.CommitmentPayments.WebJob.Updater
 
             while (true)
             {
+                //todo: break this out into component methods?
+
                 _logger.Info($"Retrieving page of data from Payment Events Service since Event Id {lastId}");
                 var stopwatch = Stopwatch.StartNew();
                 var page = (await _paymentEventsSerivce.GetDataLockEvents(lastId)).ToList();
@@ -94,55 +97,68 @@ namespace SFA.DAS.CommitmentPayments.WebJob.Updater
 
                     if (!datalockSuccess)
                     {
+                        //todo: this should be dataLockStatus.ErrorCode = ApplyXxx(x.ErrorCode)
+                        //todo: just & inside?
                         ApplyErrorCodeWhiteList(dataLockStatus);
                     }
 
+                    // if it started with no error, or it still has an error after whitelisting
                     if (datalockSuccess || dataLockStatus.ErrorCode != DataLockErrorCode.None)
                     {
                         _logger.Info($"Updating Apprenticeship {dataLockStatus.ApprenticeshipId} " +
                              $"Event Id {dataLockStatus.DataLockEventId} Status {dataLockStatus.ErrorCode}");
 
+                        //todo: determing how we can batch up calls using tvp, combined with parallel running and whenall, and error handling!
+
                         try
                         {
+                            //todo: distinct page on? group and take latest, check group for enclosing if predicate?
+                            //where ApprenticeshipId = @ApprenticeshipId
+                            //and PriceEpisodeIdentifier = @PriceEpisodeIdentifier
                             await _dataLockRepository.UpdateDataLockStatus(dataLockStatus);
 
                             await _filterAcademicYearRolloverDataLocks.Filter(dataLockStatus.ApprenticeshipId);
                         }
-                        catch(RepositoryConstraintException ex) when (_config.IgnoreDataLockStatusConstraintErrors)
+                        catch (RepositoryConstraintException ex) when (_config.IgnoreDataLockStatusConstraintErrors)
                         {
                             _logger.Warn(ex, $"Exception in DataLock updater");
                         }
 
                         if (datalockSuccess)
                         {
-                            var apprenticeship = await _apprenticeshipRepository.GetApprenticeship(dataLockStatus.ApprenticeshipId);
-                            if (!apprenticeship.HasHadDataLockSuccess)
-                            {
-                                await _apprenticeshipRepository.SetHasHadDataLockSuccess(apprenticeship.Id);
-                            }
+                            //todo: could we kick this off when we set the flag and whenall?
+                            //and the get GetPendingApprenticeshipUpdate
+                            //error handling if we do?
+                            await _apprenticeshipRepository.SetHasHadDataLockSuccess(dataLockStatus.ApprenticeshipId);
 
+                            //todo: we could combine these 2, but that would move BL into the SP
+                            //todo: GetPendingApprenticeshipUpdateCostAndTrainingCode, return in DTO, add index on AppId, Cost & TrainingCode? not worth it, won't table scan on search
                             var pendingUpdate = await
                              _apprenticeshipUpdateRepository.GetPendingApprenticeshipUpdate(dataLockStatus.ApprenticeshipId);
 
                             if (pendingUpdate != null && (pendingUpdate.Cost != null || pendingUpdate.TrainingCode != null))
                             {
+                                //todo: currently execute sql, how much performance improvement would we get by using a sp? prob not a lot for such a simple update
                                 await _apprenticeshipUpdateRepository.ExpireApprenticeshipUpdate(pendingUpdate.Id);
                                 _logger.Info($"Pending ApprenticeshipUpdate {pendingUpdate.Id} expired due to successful data lock event {dataLockStatus.DataLockEventId}");
                             }
                         }
                     }
 
+                    //when does this get written back out to the db? what else sets it? get uses MAX(DataLockEventId)
+                    //presumably events come through in DataLockEventId order
                     lastId = dataLockStatus.DataLockEventId;
                 }
             }
         }
+
 
         private void ApplyErrorCodeWhiteList(DataLockStatus dataLockStatus)
         {
             var whitelisted = DataLockErrorCode.None;
             var skipped = DataLockErrorCode.None;
 
-            foreach(DataLockErrorCode errorCode in Enum.GetValues(typeof(DataLockErrorCode)))
+            foreach (DataLockErrorCode errorCode in Enum.GetValues(typeof(DataLockErrorCode)))
             {
                 if (dataLockStatus.ErrorCode.HasFlag(errorCode))
                 {
--- src/SFA.DAS.CommitmentPayments.WebJob/Updater/FilterOutAcademicYearRollOverDataLocks.cs
+++ src/SFA.DAS.CommitmentPayments.WebJob/Updater/FilterOutAcademicYearRollOverDataLocks.cs
@@ -11,6 +11,8 @@ namespace SFA.DAS.CommitmentPayments.WebJob.Updater
     public sealed class FilterOutAcademicYearRollOverDataLocks : IFilterOutAcademicYearRollOverDataLocks
     {
         private IDataLockRepository _dataLockRepository;
+        //todo: should compile, to speed up
+        //private static Regex _augustPricePeriodFormat = new Regex(@"08\/\d{4}$", RegexOptions.Compiled);
         private static Regex _augustPricePeriodFormat = new Regex(@"08\/\d{4}$");
         private ILog _logger;
 
@@ -22,33 +24,43 @@ namespace SFA.DAS.CommitmentPayments.WebJob.Updater
 
         public async Task Filter(long apprenticeshipId)
         {
+            //todo: don't think we need to fetch the datalock we already have, so we could potentially... (does any other process update datalocks?)
+            // pass DataLockEventId of the record we already have e.g. GetDataLocksExcept(apprenticeshipId, true, dataLockStatus.xId)
+            // GetDataLocksCount and only if >1, fetch
+            // then combine existing with returned set
+
             var apprenticeshipDataLocks = await _dataLockRepository.GetDataLocks(apprenticeshipId, true);
 
-            if (apprenticeshipDataLocks == null || apprenticeshipDataLocks.Count == 0)
+            if (apprenticeshipDataLocks == null || apprenticeshipDataLocks.Count == 0) //todo: i think we should always get the one we just created/updated
                 return;
 
             var haveDuplicates = apprenticeshipDataLocks
                 .GroupBy(x => new
-                    {
-                        x.IlrTrainingCourseCode,
-                        x.IlrTrainingType,
-                        x.IlrActualStartDate,
-                        x.IlrEffectiveFromDate,
-                        x.IlrTotalCost
-                    })
+                {
+                    x.IlrTrainingCourseCode,
+                    x.IlrTrainingType,
+                    x.IlrActualStartDate,
+                    x.IlrEffectiveFromDate,
+                    x.IlrTotalCost
+                })
                 .Where(g => g.Count() > 1);
-            
-            foreach(var group in haveDuplicates)
+
+            foreach (var group in haveDuplicates)
             {
+                //todo: could use MaxBy, but would require adding MoreLINQ package. presumably we only get a very small number of duplicates??
+                //https://stackoverflow.com/questions/1101841/linq-how-to-perform-max-on-a-property-of-all-objects-in-a-collection-and-ret
                 var augustDataLock = group
                     .Select(x => new
-                        {
-                            DataLockEventId = x.DataLockEventId,
-                            PriceEpisodeIdentifier = x.PriceEpisodeIdentifier,
-                            PriceEpisodeIdDateTime = DateTime.ParseExact(x.PriceEpisodeIdentifier.Substring(x.PriceEpisodeIdentifier.Length - 10), "dd/MM/yyyy", new CultureInfo("en-GB")),
-                            IsAugustPriceEpisode = _augustPricePeriodFormat.IsMatch(x.PriceEpisodeIdentifier)
+                    {
+                        //todo: no need to store the first two
+                        DataLockEventId = x.DataLockEventId,
+                        PriceEpisodeIdentifier = x.PriceEpisodeIdentifier,
+                        PriceEpisodeIdDateTime = DateTime.ParseExact(x.PriceEpisodeIdentifier.Substring(x.PriceEpisodeIdentifier.Length - 10), "dd/MM/yyyy", new CultureInfo("en-GB")),
+                        IsAugustPriceEpisode = _augustPricePeriodFormat.IsMatch(x.PriceEpisodeIdentifier)
                     })
                     .OrderByDescending(x => x.PriceEpisodeIdDateTime).First();
+
+                //todo: is AugustPriceEpisode duplicate always guaranteed to have the latest PriceEpisodeIdDateTime?
 
                 if (!augustDataLock.IsAugustPriceEpisode)
                 {
