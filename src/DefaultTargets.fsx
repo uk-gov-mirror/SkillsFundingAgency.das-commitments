c488b393c53154f80893e246e1aa97094ab2c2aa
Paul Graham <paul.graham@amido.com>
2019-02-14 09:16:46 +0000

    try taking out the framework option

--- src/DefaultTargets.fsx
+++ src/DefaultTargets.fsx
@@ -379,7 +379,6 @@ Target "Run NUnit Tests" (fun _ ->
             {p with
                 ToolPath = nUnitRunnerLocation;
                 ShadowCopy = false;
-                Framework = Testing.NUnit3.NUnit3Runtime.Other("netcoreapp2.2");
                 ResultSpecs = [("TestResult.xml;format=" + nunitTestFormat)];
                 })
 )
