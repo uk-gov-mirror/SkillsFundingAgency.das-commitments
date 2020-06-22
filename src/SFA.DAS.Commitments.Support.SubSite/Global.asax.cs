3b824dab643a3805192a7797e13bac88391d4bd7
Shahzad <shahzad.khan@hkf-technologies.co.uk>
2020-06-22 12:47:56 +0100

    support portal changes.

--- src/SFA.DAS.Commitments.Support.SubSite/Global.asax.cs
+++ src/SFA.DAS.Commitments.Support.SubSite/Global.asax.cs
@@ -29,10 +29,10 @@ namespace SFA.DAS.Commitments.Support.SubSite
             RouteConfig.RegisterRoutes(RouteTable.Routes);
 
 
-            var siteValidatorSettings = ioc.GetService<ISiteValidatorSettings>();
+            //var siteValidatorSettings = ioc.GetService<ISiteValidatorSettings>();
 
-            GlobalConfiguration.Configuration.MessageHandlers.Add(new TokenValidationHandler(siteValidatorSettings, logger));
-            GlobalFilters.Filters.Add(new TokenValidationFilter(siteValidatorSettings, logger));
+            //GlobalConfiguration.Configuration.MessageHandlers.Add(new TokenValidationHandler(siteValidatorSettings, logger));
+            //GlobalFilters.Filters.Add(new TokenValidationFilter(siteValidatorSettings, logger));
 
 
             logger.Info("Web role started");
